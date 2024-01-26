2d0ec0ddab61bdd5f8564e702fa51066264762ee
ak774296 <arvind@studymitr.com>
2024-01-27 01:21:32 +0530

    jhh

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <main class="flex justify-center gap-4 flex-col min-h-screen">
-      <h1 class="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
+      <h1 class="text-3xl text-center font-bold underline">Kya haal hai</h1>
       <p class="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
       <img src="https://bit.ly/3wsmzTy" alt="meme" class="mx-auto" />
     </main>
